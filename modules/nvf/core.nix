a015d7e525d88c47cc4580e2d26df52dc76c40c4
abhayprabhakarannair <abhayprabhakarannair@gmail.com>
2026-05-19 21:14:10 +0530

    Some basic goodies

--- modules/nvf/core.nix
+++ modules/nvf/core.nix
@@ -11,6 +11,9 @@
     globals = {
       mapleader = " ";
       maplocalleader = " ";
+      netrw_browse_split = 0;
+      netrw_banner = 0;
+      netrw_winsize = 25;
     };
 
     extraPackages = with pkgs; [
--- modules/nvf/options.nix
+++ modules/nvf/options.nix
@@ -6,5 +6,21 @@
 
     swapfile = false;
     backup = false;
+
+    tabstop = 4;
+    softtabstop = 4;
+    shiftwidth = 4;
+    expandtab = true;
+    smartindent = true;
+
+    wrap = false;
+
+    hlsearch = false;
+    incsearch = true;
+
+    termguicolors = true;
+    scrolloff = 8;
+    signcolumn = "yes";
+    updatetime = 50;
   };
 }
